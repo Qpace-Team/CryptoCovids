17d1fbae733f49536605f82b20d4817caf23b7c7
BijanPouya <Neo.br9@gmail.com>
2021-12-02 18:09:26 +0330

    commit on master

---
+++ CryptoCovids Main/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Minter from "./components/Minter";
+import "./assets/css/style.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Minter />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
